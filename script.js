dbc024ab4c9714f00b9ed10221929eb82a7830ca
PRich57 <pcrichards57@gmail.com>
2023-08-24 09:12:51 -0600

    Create pushArray function to push user specified chars into array

--- script.js
+++ script.js
@@ -101,20 +101,20 @@ var symbols = [
   "~",
 ];
 
+// Declare object to store user preferences
+var preferences = {
+  // Number of characters
+  characters: 0,
+
+  // Boolean for upper, lower, num, special
+  isUpper: true,
+  isLower: true,
+  isNum: true,
+  isSpecial: true,
+};
+
 // Create the generatePassword function called in writePassword
 function generatePassword() {
-  // Declare object to store user preferences
-  var preferences = {
-    // Number of characters
-    characters: 0,
-
-    // Boolean for upper, lower, num, special
-    isUpper: true,
-    isLower: true,
-    isNum: true,
-    isSpecial: true,
-  };
-
   // Create function to prompt user for password length
   function userLength() {
     var x = prompt(
@@ -182,47 +182,74 @@ function generatePassword() {
   preferences.isNum = bools[2];
   preferences.isSpecial = bools[3];
 
+  console.log(preferences.isUpper);
+  console.log(preferences.isLower);
+  console.log(preferences.isNum);
+  console.log(preferences.isSpecial);
+
   // Create function that uses preferences object to create password
+  function pushArray() {
+    // Create variable to push other arrays into if true
+    charArray = [];
 
-  // Create variable to hold random password
-  var randPass;
-
-  for (var i = 0; i < preferences.characters; i++) {
-    var rU = 0;
-    var rL = 0;
-    var rN = 0;
-    var rS = 0;
-
-    // For each true charType select at least one position in the password array
-    while (preferences.isUpper && rU < 1) {
-      var rU = Math.floor(Math.random() * uppercase.length);
-      rU++;
-      console.log(rU, uppercase[rU]);
-    } 
-
-    while (preferences.isLower && rL < 1) {
-      var rL = Math.floor(Math.random() * lowercase.length);
-      rL++;
-      console.log(rL, lowercase[rL]);
-    } 
-    
-    while (preferences.isNum && rN < 1) {
-      var rN = Math.floor(Math.random() * numbers.length);
-      rN++;
-      console.log(rN, numbers[rN]);
-    } 
-    
-    while (preferences.isSpecial && rS < 1) {
-      var rS = Math.floor(Math.random() * symbols.length);
-      rS++;
-      console.log(rS, symbols[rS]);
+    if (preferences.isUpper) {
+      charArray.push(uppercase);
     }
-    // Need to get these to return their values, this isn't right
-    return [rU.value, rL.value, rN.value, rS.value];
+
+    if (preferences.isLower) {
+      charArray.push(lowercase);
+    }
+
+    if (preferences.isNum) {
+      charArray.push(numbers);
+    }
+
+    if (preferences.isSpecial) {
+      charArray.push(symbols);
+    }
+    return charArray;
   }
-  // Try to come up with a better method for this^^^^^ 
+
+  // Need to make a function that gives me at least one char from each sub-array in charArray
+  function newPass() {
+    
+  }
 
 
+  // for (var i = 0; i < preferences.characters; i++) {
+  //   var rU = 0;
+  //   var rL = 0;
+  //   var rN = 0;
+  //   var rS = 0;
+
+  //   // For each true charType select at least one position in the password array
+  //   while (preferences.isUpper && rU < 1) {
+  //     var rU = Math.floor(Math.random() * uppercase.length);
+  //     rU++;
+  //     console.log(rU, uppercase[rU]);
+  //   }
+
+  //   while (preferences.isLower && rL < 1) {
+  //     var rL = Math.floor(Math.random() * lowercase.length);
+  //     rL++;
+  //     console.log(rL, lowercase[rL]);
+  //   }
+
+  //   while (preferences.isNum && rN < 1) {
+  //     var rN = Math.floor(Math.random() * numbers.length);
+  //     rN++;
+  //     console.log(rN, numbers[rN]);
+  //   }
+
+  //   while (preferences.isSpecial && rS < 1) {
+  //     var rS = Math.floor(Math.random() * symbols.length);
+  //     rS++;
+  //     console.log(rS, symbols[rS]);
+  //   }
+  //   // Need to get these to return their values, this isn't right
+  //   return [rU.value, rL.value, rN.value, rS.value];
+  // }
+  // // Try to come up with a better method for this^^^^^
 }
 
 // Assignment Code
